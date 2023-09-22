3e77d3af24ae78b52569fa72dc1418b11eee2b2e
s.chygrai@gmail.com <s.chygrai@gmail.com>
2023-09-22 22:07:14 +0200

    Test Commit2

--- src/myapp/Temperature.java
+++ src/myapp/Temperature.java
@@ -3,7 +3,7 @@ package myapp;
 public class Temperature {
     public static void main(String [] args) {
         double  celsius, fahrenheit, kelvin;
-        celsius = 34;
+        celsius = 12;
         fahrenheit = ((celsius * 9) / 5) + 32;
         kelvin = (celsius + 273.16);
         System.out.print(celsius);
@@ -12,5 +12,6 @@ public class Temperature {
         System.out.print("F(Fahrenheit);");
         System.out.print(kelvin);
         System.out.print("K(Kelvin);");
+        System.out.print("GIT1");
     }
 }
