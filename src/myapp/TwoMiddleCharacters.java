16a0f40a8bdffb150596bcc7e24eaa3f238aa630
chygraiser <s.chygrai@gamil.com>
2023-09-29 17:07:40 +0200

    Task#5 TwoMiddleCharacters program

--- src/myapp/TwoMiddleCharacters.java
+++ src/myapp/TwoMiddleCharacters.java
@@ -3,10 +3,18 @@ package myapp;
 public class TwoMiddleCharacters {
     public static void main(String[] args) {
         String word = "Programing";
-        int length = word.length();
-
-        if (length % 2 == 0) {
-            System.out.println(word.charAt((length/2)-1) + word.charAt(length/2));
+        int left;
+        int right;
+        if (word.length() % 2 == 0)
+        {
+            left = word.length() / 2 - 1;
+            right = 2;
         }
+        else
+        {
+            left = word.length() / 2;
+            right = 1;
+        }
+        System.out.print("Two middle characters in the word Programing are : " + word.substring(left, left + right));
     }
 }
